7d8250e7677204a7ce4d8ddfffd77f326283e245
hj8473803 <8473803@qq.com>
2025-01-05 20:43:48 +0800

    sync capacitor to 6.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { registerPlugin } from '@capacitor/core';
 
-import type { WechatSDKPlugin } from './definitions';
+import  { WechatSDKPlugin } from './definitions';
 
 const WechatSDK = registerPlugin<WechatSDKPlugin>('WechatSDK', {
   web: () => import('./web').then(m => new m.WechatSDKWeb()),
